7bfb1bef2b3ffc0c064b4bbe6c3c08347f131ed9
Irfan Kanat <iekanat@gmail.com>
2021-05-24 20:34:40 +0200

    Fixed the build script

--- Render.sh
+++ Render.sh
@@ -13,7 +13,7 @@
 # for i in */*.md; do cat ./license.txt >> "$i"; done
 
 # Use Pandoc to render PDFs
-for i in */*.md; do pandoc "$i" -o "${i%.md}".pdf --template eisvogel; done 
+for i in *.md; do pandoc "$i" -o "${i%.md}".pdf --template eisvogel; done 
 for i in */*.md; do pandoc "$i" -o "${i%.md}".pdf --template eisvogel; done 
 
 # Latex Beamer
@@ -22,10 +22,20 @@ for i in */*.md; do pandoc "$i" -o "${i%.md}".pdf --template eisvogel; done
 # The presentation slide decks are created with beamer. 
 # So you need a build environment with these.
 
-for d in $(find ./ -maxdepth 1 -type  -d)
-do 
-	cd $d
-	latexmk -pdf *.tex
-	latexmk -c
-	cd ..
+dirs=($(find . -type d))
+for dir in "${dirs[@]}"; do
+  cd "$dir"
+  latexmk -pdf *.tex
+  latexmk -c
 done
+
+
+# Delete Latex artifacts
+find . -type f -name '*.aux' -print -delete
+find . -type f -name '*.fls' -print -delete
+find . -type f -name '*_latexmk' -print -delete
+find . -type f -name '*.log' -print -delete
+find . -type f -name '*.nav' -print -delete
+find . -type f -name '*.out' -print -delete
+find . -type f -name '*.snm' -print -delete
+find . -type f -name '*.toc' -print -delete
